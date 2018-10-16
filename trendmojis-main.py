97f6421069eca1b2a09bb9255bca1d6bd0c9d844
Alexis SEGURA <alex.segura06@gmail.com>
2018-10-16 21:57:57 +0200

    Change number of trends processed

--- trendmojis-main.py
+++ trendmojis-main.py
@@ -100,7 +100,7 @@ EU = "24865675"
 now = datetime.datetime.now()
 todayDate = now.strftime("%Y-%m-%d")
 start_time = time.time()
-trendDic = retrieve_trend(FRANCE, todayDate, 8)
+trendDic = retrieve_trend(FRANCE, todayDate, 7)
 tweet = write_tweet(trendDic)
 scriptDuration = time.time() - start_time
 print(scriptDuration)
